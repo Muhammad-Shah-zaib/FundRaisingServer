a97ce2a945f99f09e9a90dd135bdfd059a689916
Muhammad-Shah-zaib <mshahzab.bscs23seecs@seecs.edu.pk>
2024-05-10 14:55:00 +0500

    added the removed getcauses function

--- FundRaisingServer/Services/CauseBankService.cs
+++ FundRaisingServer/Services/CauseBankService.cs
@@ -8,6 +8,25 @@ namespace FundRaisingServer.Services;
 public class CauseBankService (FundRaisingDbContext context): ICauseBankService {
     private readonly FundRaisingDbContext _context = context;
 
+    public async Task<IEnumerable<CauseResponseDto>> GetAllCausesAsync()
+    {
+        try
+        {
+            return await this._context.Causes
+            .Select(c => new CauseResponseDto()
+            {
+                CauseDescription = c.Description ?? string.Empty,
+                CauseTitle = c.CauseTitle,
+                CollectedDonation = c.CollectedAmount
+            })
+            .ToListAsync();
+        }catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+
     public async Task<CauseBankResponseDto> GetBankAmountAsync()
     {
         try
@@ -26,16 +45,5 @@ public class CauseBankService (FundRaisingDbContext context): ICauseBankService 
         
     }
 
-    public async Task<IEnumerable<CauseResponseDto>> GetAllCausesAsync()
-    {
-        return await this._context.Causes
-            .Select(c =>new CauseResponseDto()
-            {
-                CauseTitle = c.CauseTitle,
-                CauseDescription = c.Description ?? string.Empty,
-                CollectedDonation = c.CollectedAmount
-            })
-            .ToListAsync();
-        
-    }
+
 }
