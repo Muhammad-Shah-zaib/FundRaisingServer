836076c1f64a582d6a32e0d855ffa553238cb2f3
AzkaAhmad754 <arfaat.mep@gmail.com>
2024-05-10 18:37:27 +0500

    fixed some warnings in causeUpdateDto

---
+++ FundRaisingServer/Models/DTOs/Causes/CauseUpdateDto.cs
@@ -0,0 +1,6 @@
+public class CauseUpdateDto
+{
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public int UserCnic { get; set; }
+}
